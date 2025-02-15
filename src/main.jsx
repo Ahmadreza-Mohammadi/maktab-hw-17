3562bee9a2bbe6e4e21b450fefc8066abd97987f
noway777 <ahmadrezamohammadi777@gmail.com>
2025-02-15 18:46:04 +0330

    task1 added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Task1 from './tasks/task1'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    {/* <Task1 /> */}
   </StrictMode>,
 )
--- src/tasks/task1.jsx
+++ src/tasks/task1.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 
-function Task1() {
+const Task1 = () => {
+  const inputRef = useRef(null);
+  const prevValueRef = useRef('');
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Current Value:', inputValue);
+    console.log('Previous Value:', prevValueRef.current);
+    prevValueRef.current = inputValue;
+    setInputValue('');
+  };
+
   return (
-    <div>Task1</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Input:
+        <input
+          type="text"
+          ref={inputRef}
+          value={inputValue}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-export default Task1
+export default Task1;
