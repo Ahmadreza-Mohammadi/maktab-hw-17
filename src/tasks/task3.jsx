0167628ff96dd7fe0d6176bc0140d85aa528a14f
noway777 <ahmadrezamohammadi777@gmail.com>
2025-02-16 13:21:12 +0330

    tas3 added

--- src/tasks/task3.jsx
+++ src/tasks/task3.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react';
 
-function Task3() {
+const Task3 = () => {
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  const sumOfEvens = useMemo(() => {
+    console.log('Calculating sum of even numbers...'); 
+    return numbers.reduce((sum, num) => {
+      if (num % 2 === 0) {
+        return sum + num;
+      }
+      return sum;
+    }, 0);
+  }, [numbers]);
+
+  const addRandomNumber = () => {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    setNumbers([...numbers, randomNum]);
+  };
+
   return (
-    <div>Task3</div>
-  )
-}
+    <div>
+      <h1>List of Numbers:</h1>
+      <ul>
+        {numbers.map((num, index) => (
+          <li key={index}>{num}</li>
+        ))}
+      </ul>
+      <h2>Sum of Even Numbers: {sumOfEvens}</h2>
+      <button onClick={addRandomNumber}>Add Random Number</button>
+    </div>
+  );
+};
 
-export default Task3
+export default Task3;
